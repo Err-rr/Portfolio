fc234196e0198d9f332948443137a46488a39e2f
Shivam <shivaysoni3642@gmail.com>
2026-06-29 22:26:53 +0530

    Updated the code base

--- src/constants/profileRows.js
+++ src/constants/profileRows.js
@@ -171,7 +171,7 @@ export const PROFILE_ROWS = {
       {
         title: "Do Not Open",
         image:
-          "https://images.unsplash.com/photo-1517841905240-472988babdf9?auto=format&fit=crop&w=1600&q=80",
+          "https://images.unsplash.com/photo-1557404838-0f7b750e3a04?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         link: "/stalker",
       },
     ],
